0f04a0f79366342a36894004285a55295b0e580e
Mariano Moretti <marianomoretti.87@gmail.com>
2016-11-28 20:20:09 -0300

    Download speed and download percentage added

--- renderer.js
+++ renderer.js
@@ -4,26 +4,40 @@
 
 const ExtraTorrentAPI = require('extratorrent-api');
 const peerflix = require('peerflix');
+const numeral = require('numeral');
+const request = require('request');
+const fs = require('fs');
 const proc = require('child_process');
+const subxfinder = require('subxfinder');
+const EventEmitter = require('events');
+class SubtitleEmitter extends EventEmitter {};
+const _ = require('lodash');
 const parseTorrent = require('parse-torrent');
 const et = new ExtraTorrentAPI();
 
-var app = angular.module('play-torrent', []);
-app.controller('TorrentCtrl', function ($rootScope) {
+let app = angular.module('play-torrent', []);
+app.controller('TorrentCtrl', function ($rootScope, $scope) {
+
   let torrentCtrl = this;
   torrentCtrl.term = "";
   torrentCtrl.list = [];
+  let subtitleEmitter = new SubtitleEmitter();
 
   torrentCtrl.search = () => {
     et.search(torrentCtrl.term)
       .then((response) => {
-        torrentCtrl.list = response.results;
+        torrentCtrl.list = _.orderBy(response.results, ['seeds', 'leechers', 'quality'],['desc', 'desc', 'desc']);
         $rootScope.$digest();
       })
       .catch(console.log);
   }
+
   let torrentEngine;
+
   torrentCtrl.loadTorrent = (path) => {
+    let verified = 0
+    let downloadedPercentage = 0
+
     parseTorrent.remote(path, (err, parsedTorrent) => {
       if (err) throw err
       const opts = {
@@ -34,8 +48,42 @@ app.controller('TorrentCtrl', function ($rootScope) {
 
       torrentEngine.server.on('listening', () => {
         let vlc = proc.exec(`vlc http://${opts.hostname}:${opts.port}`)
-        vlc.on('exit', () => torrentEngine.server.close())
+        vlc.on('exit', () => {
+          torrentEngine.server.close();
+          torrentEngine.destroy();
+          $scope.$apply(() => {
+            $scope.speed = numeral(0.0).format('0.0b');
+            $scope.downloaded = 0
+          })
+        })
       });
-    })
+
+      torrentEngine.on('verify', () => {
+        verified++
+        let value = verified / torrentEngine.torrent.pieces.length * 100;
+        downloadedPercentage = Math.floor(value);
+        $scope.$apply(() => {
+          $scope.speed = numeral(torrentEngine.swarm.downloadSpeed()).format('0.0b');
+          $scope.downloaded = downloadedPercentage
+        });
+      });
+
+      // const params = torrentCtrl.term.split(" ");
+      // const serie = params[0];
+      // const episode = params[1];
+      // subxfinder.searchAndFilter(serie, episode, true, (err, subtitleList) => {
+      //   if(err) console.log(err);
+      //   for (var i = 0; i < subtitleList.length; i++) {
+      //     let file = fs.createWriteStream(subtitle.title);
+      //     const subtitle = subtitleList[i];
+      //     request.get(subtitle.link)
+      //       .on('data', (chunk) => {
+
+      //       });
+      //   }
+      // });
+    });
+
+
   }
 });
